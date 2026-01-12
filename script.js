a10d9033211048d3cbac4872fe46106ab6156bb1
Finola <f.a.j.parkinson@students.uu.nl>
2026-01-12 10:39:48 +0100

    Javascript toegevoegd radi button

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 console.log("JavaScript werkt");
 
+// 1. Elementen ophalen
+const form = document.getElementById("templateForm");
+const nextBtn = document.getElementById("nextBtn");
+
+//2. veranderingen bijhouden formulier 
+
+form.addEventListener("change", function () {
+    // controleren of radio button is geselecteerd
+    const selected = form.querySelector('input[name="template"]:checked');
+
+     console.log("Geselecteerd:", selected);
+     
+    if (selected) {
+        nextBtn.disabled = false;
+        }   
+});
+
