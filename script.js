1a837be2cd2f02c1f52b0c9e275f11c85ab08b66
Finola <f.a.j.parkinson@students.uu.nl>
2026-01-12 10:44:06 +0100

    tijdelijke javascript check

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ const nextBtn = document.getElementById("nextBtn");
 //2. veranderingen bijhouden formulier 
 
 form.addEventListener("change", function () {
-    // controleren of radio button is geselecteerd
-    const selected = form.querySelector('input[name="template"]:checked');
-
-     console.log("Geselecteerd:", selected);
-     
-    if (selected) {
-        nextBtn.disabled = false;
-        }   
+  console.log("Er is iets veranderd in het formulier");
 });
 
