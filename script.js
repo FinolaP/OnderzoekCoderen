ae64af1f898d3b90516f42a0a7c1dca9db83cff3
Finola <f.a.j.parkinson@students.uu.nl>
2026-01-12 12:37:06 +0100

    Volgende knop naar leestekst

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Check of JavaScript geladen is
 console.log("JavaScript werkt");
 
+// Radio buttons en 'Volgende'-knop ophalen
 const radios = document.querySelectorAll('input[type="radio"]');
 const nextBtn = document.getElementById("nextBtn");
 
+// Activeer de knop zodra een keuze is gemaakt
 radios.forEach(function (radio) {
   radio.addEventListener("change", function () {
     console.log("Radio geselecteerd:", radio.value);
@@ -10,4 +13,7 @@ radios.forEach(function (radio) {
   });
 });
 
-//proberen
+// Ga naar de volgende pagina bij klik op de knop
+nextBtn.addEventListener("click", function () {
+  window.location.href = "leestekst.html";
+});
